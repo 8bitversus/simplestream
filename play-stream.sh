58e391239fa7380118dc548a72cc10de148e7d3a
Martin Wimpress <code@flexion.org>
2019-11-24 07:57:38 +0000

    Auto-configure WIN_TITLE

--- play-stream.sh
+++ play-stream.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+PLAYER="ffplay"
 IP_PROTO="tcp"
 IP_PORT="4864"
 IP_ADDR="127.0.0.1"
 LAUNCHER=$(basename $0 .sh)
 STAMP=$(date +"%C%j-%H%M%S")
 LOG_LEVEL="warning"
-PLAYER="ffplay"
+WIN_TITLE="${LAUNCHER} - ${PLAYER}"
 
 case ${IP_PROTO} in
   rtp)
@@ -28,10 +29,10 @@ if [ "${LAUNCHER}" == "play-stream" ]; then
       # - https://stackoverflow.com/questions/16658873/how-to-minimize-the-delay-in-a-live-streaming-with-ffmpeg
       echo "Playing: ${IP_PROTO}://${IP_ADDR}:${IP_PORT}${STREAM_OPTIONS}"
       ffplay -hide_banner -threads 0 -loglevel ${LOG_LEVEL} -stats \
-        -fflags nobuffer+fastseek+flush_packets -flags low_delay -sync ext -framedrop -window_title "${LAUNCHER} - ffplay" -i "${IP_PROTO}://${IP_ADDR}:${IP_PORT}${STREAM_OPTIONS}"
+        -fflags nobuffer+fastseek+flush_packets -flags low_delay -sync ext -framedrop -window_title "${WIN_TITLE}" -i "${IP_PROTO}://${IP_ADDR}:${IP_PORT}${STREAM_OPTIONS}"
       ;;  
     mpv)
-      mpv --no-cache --untimed --profile=low-latency --title="${LAUNCHER} - mpv" "${IP_PROTO}://${IP_ADDR}:${IP_PORT}${STREAM_OPTIONS}"
+      mpv --no-cache --untimed --profile=low-latency --title="${WIN_TITLE}" "${IP_PROTO}://${IP_ADDR}:${IP_PORT}${STREAM_OPTIONS}"
       ;;
   esac
 elif [ "${LAUNCHER}" == "record-stream" ]; then
