68280173f32433486f951f0f9dc427c0c01fb6a0
Martin Wimpress <code@flexion.org>
2019-12-14 13:07:20 +0000

    C64 uses the C64HQ palette.

--- emulate-commodore.sh
+++ emulate-commodore.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-x64 --default -hwscalepossible -joydev1 4 -joydev2 5 "$@"
+x64 --default -hwscalepossible -joydev1 4 -joydev2 5 -VICIIextpal -VICIIpalette /usr/lib/vice/C64/c64hq.vpl "$@"
