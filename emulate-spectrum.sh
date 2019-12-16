2d490434cc225ac2cf315f947ee547410a0a22f1
Martin Wimpress <code@flexion.org>
2019-12-16 20:24:00 +0000

    Make sure FUSE starts with sane defaults

--- emulate-spectrum.sh
+++ emulate-spectrum.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-cp configs/zxspectrum48k.conf ${HOME}/.fuserc
-fuse-gtk -g paltv2x --pal-tv2x "$@"
+fuse-gtk --graphics-filter paltv2x \
+         --joystick-1-output 2 \
+         --joystick-2-output 2 \
+         --kempston \
+         --machine 48 \
+         --pal-tv2x \
+         --sound \
+         --volume-beeper 10 \
+         "$@"
