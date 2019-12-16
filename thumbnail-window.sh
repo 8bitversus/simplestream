278e61f66b50d9addfbd9e0a22086b76b1447ea5
Martin Wimpress <code@flexion.org>
2019-12-16 13:33:21 +0000

    Make the script what it is; a thumbnailer.

--- thumbnail-window.sh
+++ thumbnail-window.sh
@@ -3,7 +3,7 @@
 # Get the window we want to resize
 # - https://unix.stackexchange.com/questions/14159/how-do-i-find-the-window-dimensions-and-position-accurately-including-decoration
 TMP_XWININFO=$(mktemp -u)
-echo -e "Please select the window you\nwould like to resize by clicking the\nmouse in that window."
+echo -e "Please select the window you\nwould like to resize & thumbnail by clicking the\nmouse in that window."
 xwininfo | tee ${TMP_XWININFO} > /dev/null
 WINDOW_ID=$(grep "Window id:" ${TMP_XWININFO} | head -n1 | cut -d':' -f3 | cut -d' ' -f2)
 rm -f ${TMP_XWININFO}
@@ -13,3 +13,8 @@ echo "Resizing: ${WINDOW_ID}"
 wmctrl -i -r ${WINDOW_ID} -e 0,0,0,1280,720
 # Raise the window
 wmctrl -i -R ${WINDOW_ID}
+if [ -e $(which mate-screenshot) ]; then
+  mate-screenshot --window --remove-border
+else
+  echo "WARNING! No supported screenshot tool found."
+fi
