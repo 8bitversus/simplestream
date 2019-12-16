8b6ccc9ade5e0f661341d2aa434b7091f5ae9ceb
Martin Wimpress <code@flexion.org>
2019-12-16 13:45:14 +0000

    Use variables to define thumbnail size

--- thumbnail-window.sh
+++ thumbnail-window.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+RES_X=1280
+RES_Y=720
+
 # Get the window we want to resize
 # - https://unix.stackexchange.com/questions/14159/how-do-i-find-the-window-dimensions-and-position-accurately-including-decoration
 TMP_XWININFO=$(mktemp -u)
@@ -8,9 +11,9 @@ xwininfo | tee ${TMP_XWININFO} > /dev/null
 WINDOW_ID=$(grep "Window id:" ${TMP_XWININFO} | head -n1 | cut -d':' -f3 | cut -d' ' -f2)
 rm -f ${TMP_XWININFO}
 
-echo "Resizing: ${WINDOW_ID}"
+echo "Resizing: ${WINDOW_ID} to ${RES_X}x${RES_Y}"
 # Set the resolution
-wmctrl -i -r ${WINDOW_ID} -e 0,0,0,1280,720
+wmctrl -i -r ${WINDOW_ID} -e 0,0,0,${RES_X},${RES_Y}
 # Raise the window
 wmctrl -i -R ${WINDOW_ID}
 if [ -e $(which mate-screenshot) ]; then
