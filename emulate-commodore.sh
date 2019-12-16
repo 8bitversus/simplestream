d246f30fc3a9eaee7c2ad403be0bf204d08283f9
Martin Wimpress <code@flexion.org>
2019-12-16 07:52:18 +0000

    Drop C64HQ palette and tweak the VICII CRT instead
    The palette tweaks result in some unusual colours. So instead of changing the palette entirely I've tweak the CRT settings of the default internal palette to make the colours more vibrant while staying true to the C64.

--- emulate-commodore.sh
+++ emulate-commodore.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-x64 --default -hwscalepossible -joydev1 4 -joydev2 5 -VICIIextpal -VICIIpalette /usr/lib/vice/C64/c64hq.vpl "$@"
+x64 -default \
+  -pal \
+  -joydev1 4 \
+  -joydev2 5 \
+  -VICIIdsize \
+  -VICIIdscan \
+  -VICIIhwscale \
+  -VICIIfilter 1 \
+  -VICIIintpal \
+  -VICIIsaturation 1600 \
+  -VICIIcontrast 1400 \
+  -VICIIbrightness 1100 \
+  -VICIIgamma 3600 \
+  -VICIItint 1100 \
+  -VICIIcrtblur 0 \
+  -VICIIcrtscanlineshade 650 \
+  -VICIIoddlinesphase 1000 \
+  -VICIIoddlinesoffset 1100 \
+  "$@"
