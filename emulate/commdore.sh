121f5c094e8bbdcfb7707f1b0bcd3d4734fe5671
Martin Wimpress <code@flexion.org>
2019-12-22 02:11:26 +0000

    Add options to emulator launchers
    Rationalise emulator launchers. Add options for common tunables.

---
+++ emulate/commdore.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# The purpose of this script is to launch VICE emualting a "breadbin"
+# Commodore 64 in a predictable way for streaming. We're based in the UK so
+# keen to emulate PAL refresh rates and timings to recreate (as best we can)
+# the computers we owned and loved in the 1980s.
+
+MODEL="c64"
+VOLUME="20"
+BORDERS="3"
+
+function usage {
+  echo "HELP! There is no help here. Ask Wimpy!"
+  exit 1
+}
+
+# Check for optional parameters
+while [ $# -gt 0 ]; do
+  case "${1}" in
+    -borders|--borders)
+      BORDERS="0"
+      shift;;
+    -volume|--volume)
+      VOLUME="$2"
+      shift
+      shift;;
+    -model|--model)
+      MODEL="$2"
+      shift
+      shift;;
+    -h|--h|-help|--help)
+      usage;;
+    *)
+      echo "ERROR! \"${1}\" is not a supported parameter."
+      usage;;
+  esac
+done
+
+if [ "${MODEL}" != "c64" ]; then
+  echo "ERROR! Unknown machine type: ${MODEL}. Quitting."
+  exit 1
+fi
+
+if [ "${BORDERS}" != "0" ] && [ "${BORDERS}" != "3" ]; then
+  echo "ERROR! Unknown border type: ${BORDERS}. Quitting."
+  exit 1
+fi
+
+x64 -default \
+  -model ${MODEL} \
+  -pal \
+  -joydev1 4 \
+  -joydev2 5 \
+  -keepaspect \
+  -refresh 1 \
+  -soundvolume "${VOLUME}" \
+  +trueaspect \
+  -VICIIborders "${BORDERS}" \
+  -VICIIdsize \
+  -VICIIdscan \
+  -VICIIhwscale \
+  -VICIIfilter 1 \
+  -VICIIintpal \
+  -VICIIsaturation 1600 \
+  -VICIIcontrast 1400 \
+  -VICIIbrightness 1100 \
+  -VICIIgamma 3600 \
+  -VICIItint 1100 \
+  -VICIIcrtblur 0 \
+  -VICIIcrtscanlineshade 650 \
+  -VICIIoddlinesphase 1000 \
+  -VICIIoddlinesoffset 1100 \
+  "$@"
---
+++ emulate/spectrum.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# The purpose of this script is to launch FUSE emualting a ZX Spectrum 48k
+# in a predictable way for streaming. We're based in the UK so keen to
+# emulate PAL refresh rates and timings to recreate (as best we can) the 
+# computers we owned and loved in the 1980s.
+
+if [ -x /usr/bin/fuse-gtk ]; then
+  FUSE="/usr/bin/fuse-gtk"
+elif [ -x /usr/bin/fuse-sdl ]; then
+  FUSE="/usr/bin/fuse-sdl"
+else
+  FUSE=""
+fi
+
+MACHINE="48"
+VOL_BEEPER="20"
+
+function usage {
+  echo "HELP! There is no help here. Ask Wimpy!"
+  exit 1
+}
+
+# Check for optional parameters
+while [ $# -gt 0 ]; do
+  case "${1}" in
+    -128|--128)
+      MACHINE="128"
+      shift;;
+    -48|--48)
+      MACHINE="48"
+      shift;;
+    -beeper|--beeper)
+      VOL_BEEPER="$2"
+      shift
+      shift;;
+    -machine|--machine)
+      MACHINE="$2"
+      shift
+      shift;;
+    -h|--h|-help|--help)
+      usage;;
+    *)
+      echo "ERROR! \"${1}\" is not a supported parameter."
+      usage;;
+  esac
+done
+
+if [ ! -e "${FUSE}" ]; then
+  echo "ERROR! Could not find FUSE. Quitting."
+  exit 1
+fi
+
+if [ "${MACHINE}" != "48" ] && [ "${MACHINE}" != "128" ]; then
+  echo "ERROR! Unknown machine type: ${MACHINE}. Quitting."
+  exit 1
+fi
+
+${FUSE} --graphics-filter paltv2x \
+         --joystick-1-output 2 \
+         --joystick-2-output 2 \
+         --kempston \
+         --machine "${MACHINE}" \
+         --pal-tv2x \
+         --sound \
+         --volume-beeper "${VOL_BEEPER}" \
+         "$@"
